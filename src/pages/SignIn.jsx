4b170e02ff4c3f889dc0da9fb4e068d6c9eee66c
caliskanadem <ad3m.calikan@gmail.com>
2023-07-04 22:35:55 +0100

    Worked on SignIn & SignUp pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import {
+  TextField,
+  CssBaseline,
+  Button,
+  Box,
+  Container,
+  Link,
+  Avatar,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <Container maxWidth="xs">
+        <Box component="form" sx={{ mt: 4 }}>
+          <Avatar
+            sx={{
+              mx: "auto",
+              backgroundColor: "primary.main",
+            }}
+          >
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h4" sx={{ textAlign: "center" }}>
+            Sign In
+          </Typography>
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Email Address"
+            required
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Password"
+            required
+            type="password"
+          />
+          <Button variant="contained" fullWidth sx={{ marginTop: 2 }}>
+            Sign In
+          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: 4,
+            }}
+          >
+            <Link>Forgot Password?</Link>
+            <Link>Don't have an account? Sign Up</Link>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import {
+  TextField,
+  CssBaseline,
+  Button,
+  Box,
+  Container,
+  Link,
+  Avatar,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <Container maxWidth="xs">
+        <Box component="form" sx={{ mt: 4 }}>
+          <Avatar
+            sx={{
+              mx: "auto",
+              backgroundColor: "primary.main",
+            }}
+          >
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h4" sx={{ textAlign: "center" }}>
+            Sign Up
+          </Typography>
+
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Full Name"
+            required
+            autoComplete="name"
+            autoFocus
+          />
+
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Email Address"
+            required
+            autoComplete="email"
+          />
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Password"
+            required
+            type="password"
+          />
+          <Button variant="contained" fullWidth sx={{ marginTop: 2 }}>
+            Sign In
+          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              marginTop: 4,
+            }}
+          >
+            <Link>Already have an Account? Sign In</Link>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default SignUp;
